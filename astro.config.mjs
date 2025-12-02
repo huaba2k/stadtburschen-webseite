b35098955d6dbb403d5d76efeaeeb7507229bda4
huaba2k <tobias@huber-fs.de>
2025-12-02 14:54:46 +0100

    CSS Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  // WICHTIG: Hier deine exakte URL eintragen (ohne Slash am Ende)
   site: 'https://new.stadtburschen.de',
-  
   integrations: [tailwind()],
+  build: {
+    // Ändert den Ordner von "_astro" zu "assets"
+    assets: 'assets'
+  }
 });
